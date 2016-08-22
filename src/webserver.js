fdea498fa0729439faa7d4781a07e39775602173
Joel Wallis Jucá <joelwallis@gmail.com>
2016-08-22 16:03:21 -0300

    Adaptation for Heroku deployment

--- src/webserver.js
+++ src/webserver.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 8080
 
-app.use(express.static('www'))
+app.use(express.static(`${__dirname}/../www`))
 
-app.listen(8080, () => console.log('Listening on 8080'))
+app.listen(port, () => console.log(`Listening on ${port}`))
